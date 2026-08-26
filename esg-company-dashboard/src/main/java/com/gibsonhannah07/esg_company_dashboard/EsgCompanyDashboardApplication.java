338a91eab588c025ae901f1061de3dc30123e2ad
Hannah <gibsonhannah07@gmail.com>
2026-08-26 18:25:28 -0500

    Added spring boot project

---
+++ esg-company-dashboard/src/main/java/com/gibsonhannah07/esg_company_dashboard/EsgCompanyDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.gibsonhannah07.esg_company_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgCompanyDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgCompanyDashboardApplication.class, args);
+	}
+
+}
---
+++ esg-company-dashboard/src/test/java/com/gibsonhannah07/esg_company_dashboard/EsgCompanyDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gibsonhannah07.esg_company_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgCompanyDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
