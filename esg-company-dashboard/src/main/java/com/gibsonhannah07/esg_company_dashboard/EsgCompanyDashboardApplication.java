727589eeba190659d7c9a3e9c42f5d533ffc83b7
Hannah <gibsonhannah07@gmail.com>
2026-09-09 19:34:02 -0500

    added createdBy relationship, timestamp, and favorite models and removed commandlinerunner from main app

--- esg-company-dashboard/src/main/java/com/gibsonhannah07/esg_company_dashboard/EsgCompanyDashboardApplication.java
+++ esg-company-dashboard/src/main/java/com/gibsonhannah07/esg_company_dashboard/EsgCompanyDashboardApplication.java
@@ -2,11 +2,6 @@ package com.gibsonhannah07.esg_company_dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.CommandLineRunner;
-import com.gibsonhannah07.esg_company_dashboard.repository.UserRepository;
-import com.gibsonhannah07.esg_company_dashboard.model.User;
-
 
 @SpringBootApplication
 public class EsgCompanyDashboardApplication {
@@ -14,13 +9,4 @@ public class EsgCompanyDashboardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EsgCompanyDashboardApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner test(UserRepository repo) {
-		return args -> {
-			repo.save(new User("Hannah"));
-			System.out.println("Saved user!");
-		};
-	}
 }
-
